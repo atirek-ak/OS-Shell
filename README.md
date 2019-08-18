11955f4dea48dbf31ea4116f4c9a997f95090856
Atirek Kumar <atirek.kumar@research.iiit.ac.in>
2019-08-18 17:22:22 +0530

    Created repository

---
+++ README.md
@@ -0,0 +1,1 @@
+Description: OS bash shell in C
---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <dirent.h>
+// #include <pwd.h>
+// #include <time.h>
+// #include <grp.h>
+// #include <sys/wait.h>
+// #include "headers.h"
+// #include <fcntl.h> 
+
+void initiate()
+{
+	
+}
+
+int main(int argc, char  *argv[])
+{
+	initiate();
+	return EXIT_SUCCESS;
+}
